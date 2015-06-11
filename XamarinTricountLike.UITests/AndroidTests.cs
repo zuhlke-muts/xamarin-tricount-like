bdd0b42dc2652394e91ad885341fcf1dcc680de6
Roman Scheidegger <roman.scheidegger@zuehlke.com>
2015-06-11 10:28:05 +0200

    added Xamarin UITests project

---
+++ XamarinTricountLike.UITests/AndroidTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Utils;
+
+namespace XamarinTricountLike.UITests
+{
+    public class WaitTimes : IWaitTimes
+    {
+        public TimeSpan WaitForTimeout
+        {
+            get { return TimeSpan.FromSeconds(120); }
+        }
+
+        public TimeSpan GestureWaitTimeout
+        {
+            get { return TimeSpan.FromSeconds(10); }
+        }
+    }
+
+    [TestFixture]
+    public class AndroidTests : CrossPlatformTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            // an API key is required to publish on Xamarin Test Cloud for remote, multi-device testing
+            App = ConfigureApp.Android.ApkFile(PathToApk).DeviceSerial("019314f0").WaitTimes(new WaitTimes()).StartApp();
+        }
+
+        public string PathToApk { get; set; }
+
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetup()
+        {
+            string currentFile = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+            var fi = new FileInfo(currentFile);
+            string dir = fi.Directory.Parent.Parent.Parent.FullName;
+            // update the project name (Android) and output filename (UITestDemo.Android) for each app
+            PathToApk = Path.Combine(dir, "XamarinTricountLike", "XamarinTricountLike.Droid", "bin", "Debug",
+                "zuehlke.muts.camp.apk");
+        }
+    }
+}
---
+++ XamarinTricountLike.UITests/CrossPlatformTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace XamarinTricountLike.UITests
+{
+    public class CrossPlatformTests
+    {
+        protected IApp App;
+
+        static readonly Func<AppQuery, AppQuery> AddButton = c => c.Marked("AddButton");
+        static readonly Func<AppQuery, AppQuery> SaveButton = c => c.Marked("SaveButton").Text("Save");
+
+        /// <summary>
+        /// This test checks the value of a Label, presses a AddButton, then checks the Label text again
+        /// </summary>
+        [Test()]
+        public void TestCase()
+        {
+            // Arrange
+            
+            // Act
+            App.Tap(AddButton);
+
+            // Assert
+            var result = App.Query(SaveButton);
+            Assert.IsTrue(result.Any(), "SaveButton not shown");
+        }
+    }
+}
